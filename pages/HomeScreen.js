15715d67cab2cb1dc5a67d07a5ad1bfdff7855f1
yusuf <ysfozturk3@gmail.com>
2020-12-21 21:26:27 +0100

    created a example with rn-sql-storage

---
+++ pages/HomeScreen.js
@@ -0,0 +1,80 @@
+import React, { useEffect } from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+import Mybutton from './components/Mybutton';
+import Mytext from './components/Mytext';
+import { openDatabase } from 'react-native-sqlite-storage';
+
+var db = openDatabase({ name: 'UserDatabase.db' });
+
+const HomeScreen = ({ navigation }) => {
+  useEffect(() => {
+    db.transaction(function (txn) {
+      txn.executeSql(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='table_user'",
+        [],
+        function (tx, res) {
+          console.log('item:', res);
+          if (res.rows.length == 0) {
+            txn.executeSql('DROP TABLE IF EXISTS table_user', []);
+            txn.executeSql(
+              'CREATE TABLE IF NOT EXISTS table_user(user_id INTEGER PRIMARY KEY AUTOINCREMENT, user_name VARCHAR(20), user_contact INT(10), user_address VARCHAR(255), image BLOB)',
+              []
+            );
+          }
+        }
+      );
+    });
+  }, []);
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: 'white' }}>
+        <View style={{ flex: 1 }}>
+          <Mytext text="SQLite Example" />
+          <Mybutton
+            title="Register"
+            customClick={() => navigation.navigate('Register')}
+          />
+          <Mybutton
+            title="Update"
+            customClick={() => navigation.navigate('Update')}
+          />
+          <Mybutton
+            title="View"
+            customClick={() => navigation.navigate('View')}
+          />
+          <Mybutton
+            title="View All"
+            customClick={() => navigation.navigate('ViewAll')}
+          />
+          <Mybutton
+            title="Delete"
+            customClick={() => navigation.navigate('Delete')}
+          />
+          <Mybutton
+            title="image"
+            customClick={() => navigation.navigate('Image')}
+          />
+        </View>
+        <Text
+          style={{
+            fontSize: 18,
+            textAlign: 'center',
+            color: 'grey'
+          }}>
+          Example of SQLite Database in React Native
+        </Text>
+        <Text
+          style={{
+            fontSize: 16,
+            textAlign: 'center',
+            color: 'grey'
+          }}>
+          www.aboutreact.com
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
